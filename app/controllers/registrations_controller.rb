d909fa145cae4e62649a881ebd75365e975d1708
Adam Hobson <adam@huntsmanlabs.com>
2015-10-14 14:30:23 +1100

    Added timezone to user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,11 @@ class RegistrationsController < Devise::RegistrationsController
 	private
 
 	def sign_up_params
-		params.require(user).permit(:email, :password, :time_zone)
+		params.require(:user).permit(:email, :password, :time_zone)
 	end
 
 	def account_update_params
-		params.require(user).permit(:email, :password, :time_zone, :current_password)
+		params.require(:user).permit(:email, :password, :time_zone, :current_password)
 	end
 
 end
